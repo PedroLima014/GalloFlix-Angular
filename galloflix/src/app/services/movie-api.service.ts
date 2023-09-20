e0256da426b67f7bea98ca79c4e95097924d209e
Pedro h <pedro.henrique.lima0205@gmail.com>
2023-09-19 22:24:34 -0300

    Aula 19-09

--- galloflix/src/app/services/movie-api.service.ts
+++ galloflix/src/app/services/movie-api.service.ts
@@ -10,9 +10,12 @@ export class MovieApiService {
   constructor(private http: HttpClient) { }
 
   baseUrl = "https://api.themoviedb.org/3";
-  apiKey = "<f48f98ce3696eff01e2389a65a0a4745>";
+  apiKey = "f48f98ce3696eff01e2389a65a0a4745";
 
   bannerApiData(): Observable<any> {
-    return this.http.get('${this.baseUrl}/trending/all/week?api key=${this.apikey}&language=pt-BR'); 
+    return this.http.get(`${this.baseUrl}/trending/all/week?api_key=${this.apiKey}&language=pt-Br`); 
+  }
+  trendingMovieApiData(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/trending/movie/day?api_key=${this.apiKey}&language=pt-BR`);
   }
 }
